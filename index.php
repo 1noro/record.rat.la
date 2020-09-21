5a0b325f86b28dc2f093edb69427f9f69eb6de1f
1noro <ppuubblliicc@protonmail.com>
2020-09-21 12:51:39 +0200

    meta tags reestructuradas

--- index.php
+++ index.php
@@ -187,23 +187,23 @@
         <!-- Revisar: https://css-tricks.com/essential-meta-tags-social-media/ -->
         <meta name="author" content="Inoro"> <!-- This site was made by https://github.com/1noro -->
         <meta name="description" content="<?php echo $description; ?>">
-        <meta name="robots" content="index, follow" />
-        <!-- <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
-        <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" /> -->
-        <link rel="canonical" href="<?php echo get_url(true); ?>" />
         <meta property="og:locale" content="es_ES" />
         <meta property="og:type" content="article" />
         <meta property="og:title" content="<?php echo $title; ?>" />
         <meta property="og:description" content="<?php echo $description; ?>" />
         <meta property="og:url" content="<?php echo get_url(true); ?>" />
         <meta property="og:site_name" content="record.rat.la" />
+        <meta property="og:image" content="<?php echo get_url(false) . "/" . $article_img; ?>" />
+        <meta name="twitter:card" content="summary_large_image" />
         <!-- <meta property="article:author" content="idex.php?q=202009180002i-inoro.html" /> -->
         <!-- <meta property="article:published_time" content="2020-09-21T00:04:15+00:00" /> -->
         <!-- <meta property="article:modified_time" content="2020-09-21T07:23:04+00:00" /> -->
-        <meta property="og:image" content="<?php echo get_url(false) . "/" . $article_img; ?>" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <!-- <meta name="twitter:creator" content="@https://twitter.com/0x12Faab7" />
-        <meta name="twitter:site" content="@0x12Faab7" /> -->
+        <!-- <meta name="twitter:creator" content="@https://twitter.com/0x12Faab7" /> -->
+        <!-- <meta name="twitter:site" content="@0x12Faab7" /> -->
+        <meta name="robots" content="index, follow" />
+        <!-- <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" /> -->
+        <!-- <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" /> -->
+        <!-- <link rel="canonical" href="<?php echo get_url(true); ?>" /> -->
 
         <style>
             body {
