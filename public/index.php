b5e854b342c5ea60268c5effc2fcbc945f3e7317
1noro <ppuubblliicc@protonmail.com>
2020-09-28 14:55:40 +0200

    gif --> svg

--- public/index.php
+++ public/index.php
@@ -278,7 +278,7 @@
         <header>
             <h1>record.rat.la</h1>
             <!-- Para evitar que el contenido se mueva al cargar la imagen puse "height: 209px;" al <p>. -->
-            <p style="height: 209px;"><a href="https://www.instagram.com/pepunto.reik" title="Artista: @pepunto.reik"><img src="img/rat_ajuste2_400<?php echo $colors[$color_id]["hedaer_img_color"]; ?>.webp" alt="Imagen del header, rata cantando: lalala."></a></p>
+            <p style="height: 209px;"><a href="https://www.instagram.com/pepunto.reik" title="Artista: @pepunto.reik"><img src="img/rat<?php echo $colors[$color_id]["hedaer_img_color"]; ?>.svg" alt="Imagen del header, rata cantando: lalala."></a></p>
             <!-- Licencia de la imagen -->
             <script type="application/ld+json">
                 {
