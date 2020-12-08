29fbc214551c2f0ad16135d21830f4c5b790376d
1noro <ppuubblliicc@protonmail.com>
2020-12-08 14:40:01 +0100

    probando otro tipo de imagenes

--- public/index.php
+++ public/index.php
@@ -382,7 +382,7 @@
             <!-- Para evitar que el contenido se mueva al cargar la imagen puse "height: 180.47px;" al <p>. -->
             <p><!-- style="height: 180.47px;" -->
                 <a href="https://www.instagram.com/pepunto.reik" aria-label="Artista: @pepunto.reik">
-                    <img src="img/rat<?php echo $colors[$color_id]["hedaer_img_color"]; ?>.png" alt="Logotipo de la web, una rata cantando: la la la." width="400" height="180.47">
+                    <img src="img/rat<?php echo $colors[$color_id]["hedaer_img_color"]; ?>.webp" alt="Logotipo de la web, una rata cantando: la la la." width="400" height="180.47">
                 </a>
                 <!-- Licencia de la imagen -->
                 <script type="application/ld+json">
