4e5220d44fab1f5de690b4aa2554a62b59834fa2
1noro <ppuubblliicc@protonmail.com>
2021-11-07 20:12:36 +0100

    google cookie samesite warning elimination

--- public/index.php
+++ public/index.php
@@ -533,7 +533,9 @@
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'UA-179041248-1');
+            gtag('config', 'UA-179041248-1', {
+                cookie_flags: 'max-age=7200;secure;samesite=none'
+            });
         </script>
 
         <style>
