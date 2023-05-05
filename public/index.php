2bddc2dd8f04ca4ccc67267333b51c57a44baa47
1noro <ppuubblliicc@protonmail.com>
2023-05-05 19:27:49 +0200

    feat: acabado de configurar el redirect

--- public/index.php
+++ public/index.php
@@ -1,13 +1,35 @@
 <?php
 
+// --- Funciones ---
+/**
+ * get_filenames, obtiene los nombres de las páginas en la carpeta
+ * especificada
+ * 
+ * @return array<string>
+ */
+function get_filenames(string $directory) : array {
+    $filenames = [];
+    $directoryObj = opendir($directory) ?: null;
+    while($filename = readdir($directoryObj)) {
+        if(($filename != ".") && ($filename != "..")) {
+            // push value to array
+            $filenames[] = $filename;
+        }
+    }
+    return $filenames;
+}
+
 // --- Constantes ---
+define("POST_FOLDER", "pages/posts/"); // carpeta donde se guardan las páginas
 define("COMMON_FOLDER", "pages/common/"); // carpeta donde se guardan las páginas
 define("POST_FILENAMES", get_filenames(POST_FOLDER)); // obtenemos todas las páginas de la carpeta POST_FOLDER
 
+define("DEF_AUTHOR_USER_NAME", "anon"); // datos de autor por defecto
+
 // autor por defecto: Anon
 define("AUTHORS", [
-    DEF_AUTHOR_USER_NAME => new Author(DEF_AUTHOR_USER_NAME, "Anon", "anon.html"),
-    "inoro" => new Author("inoro", "Inoro", "inoro.html")
+    DEF_AUTHOR_USER_NAME => 0,
+    "inoro" => 0
 ]);
 
 // --- Variables globales ---
@@ -29,8 +51,48 @@ if ('/' === $uri) {
         // $page = new ContentPage($path, get_full_uri());
         if ($_GET['filename'] == 'mi-software.html') {
             $locationURL = "https://rats.land/mi-software/";
-        } else if ($_GET['filename'] == '') {
-            $locationURL = "https://rats.land/";
+        } else if ($_GET['filename'] == 'fallen-angels-wong-kar-wai.html') {
+            $locationURL = "https://rats.land/post/fallen-angels-wong-kar-wai/";
+        } else if ($_GET['filename'] == 'estoy-leyendo-sobre-estoicismo.html') {
+            $locationURL = "https://rats.land/post/estoy-leyendo-sobre-estoicismo/";
+        } else if ($_GET['filename'] == 'le-sommet-des-dieux.html') {
+            $locationURL = "https://rats.land/post/le-sommet-des-dieux/";
+        } else if ($_GET['filename'] == 'la-cinta-blanca-michael-haneke.html') {
+            $locationURL = "https://rats.land/post/la-cinta-blanca-michael-haneke/";
+        } else if ($_GET['filename'] == 'emocionado-link-blog.html') {
+            $locationURL = "https://rats.land/post/emocionado-link-blog/";
+        } else if ($_GET['filename'] == 'cd-fuzzy-finder.html') {
+            $locationURL = "https://rats.land/post/cd-fuzzy-finder/";
+        } else if ($_GET['filename'] == 'expandir-ext4-dinamicamente.html') {
+            $locationURL = "https://rats.land/post/expandir-ext4-dinamicamente/";
+        } else if ($_GET['filename'] == 'haxan-brujeria-a-traves-del-tiempo.html') {
+            $locationURL = "https://rats.land/post/haxan-brujeria-a-traves-del-tiempo/";
+        } else if ($_GET['filename'] == 'yojimbo-japon-inspira-western.html') {
+            $locationURL = "https://rats.land/post/yojimbo-japon-inspira-western/";
+        } else if ($_GET['filename'] == 'aranas-antiguas-sueno-esoterico.html') {
+            $locationURL = "https://rats.land/post/aranas-antiguas-sueno-esoterico/";
+        } else if ($_GET['filename'] == 'comandos-utiles-git.html') {
+            $locationURL = "https://rats.land/post/comandos-utiles-git/";
+        } else if ($_GET['filename'] == 'drivers-nvidia-arch-linux.html') {
+            $locationURL = "https://rats.land/post/drivers-nvidia-arch-linux/";
+        } else if ($_GET['filename'] == 'gitlab-nginx.html') {
+            $locationURL = "https://rats.land/post/gitlab-nginx/";
+        } else if ($_GET['filename'] == 'solucion-series-numericas-java-python.html') {
+            $locationURL = "https://rats.land/post/series-numericas-en-java-y-python/";
+        } else if ($_GET['filename'] == 'la-forma-mas-rapida-de-hacer-un-git-push.html') {
+            $locationURL = "https://rats.land/post/la-forma-mas-rapida-de-hacer-un-git-push/";
+        } else if ($_GET['filename'] == 'cowboy-bebop.html') {
+            $locationURL = "https://rats.land/post/cowboy-bebop/";
+        } else if ($_GET['filename'] == 'habilitar-http2-nginx.html') {
+            $locationURL = "https://rats.land/post/habilitar-http2-nginx/";
+        } else if ($_GET['filename'] == 'configurando-nginx-para-esta-web.html') {
+            $locationURL = "https://rats.land/post/un-servidor-para-record-rat-la/";
+        } else if ($_GET['filename'] == 'los-buenos-dias-en-los-chats.html') {
+            $locationURL = "https://rats.land/post/los-buenos-dias-en-los-chats-on-line/";
+        } else if ($_GET['filename'] == 'ubiquiti-er-movistartv.html') {
+            $locationURL = "https://github.com/1noro/ubiquiti-er-movistartv";
+        } else if ($_GET['filename'] == 'genesis.html') {
+            $locationURL = "https://rats.land/post/genesis/";
         } else {
             $locationURL = 'https://rats.land/post/';
         }
@@ -55,11 +117,11 @@ if ('/' === $uri) {
 } elseif ('/faq' === $uri) {
     $locationURL = "https://rats.land/faq/";
 } elseif ('/donations' === $uri) {
-    $locationURL = "https://rats.land/donations/";
+    $locationURL = "https://rats.land/donaciones/";
 } elseif ('/description' === $uri) {
     $locationURL = "https://rats.land/info/";
 } elseif ('/cookie' === $uri) {
-    $locationURL = $locationURL_error;
+    $locationURL = "https://rats.land/";
 } else {
     // Error 404 (Page not found)
     $locationURL = $locationURL_error;
@@ -71,7 +133,11 @@ if ($ACTION == 404) {
     http_response_code(404);
 }
 
+echo '<h1>record.rat.la se muda a <a href="https://rats.land/">rats.land</a></h1>';
+echo 'Redirigiendo a <a href="' . $locationURL . '">' . $locationURL . '</a>...';
+
 // header("HTTP/1.1 301 Moved Permanently");
-http_response_code(301);
-header("Location: $locationURL");
+
+// http_response_code(301);
+// header("Location: $locationURL");
 exit();
